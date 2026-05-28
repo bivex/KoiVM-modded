d24e689f0811c4e4636a9791563c2f0a7d83bd0e
Kilo <209128140+bivex@users.noreply.github.com>
2026-05-29 02:16:35 +0300

    Update KoiHeap.cs

--- KoiVM/RT/KoiHeap.cs
+++ KoiVM/RT/KoiHeap.cs
@@ -13,7 +13,7 @@ namespace KoiVM.RT
         private readonly List<byte[]> chunks = new List<byte[]>();
         private uint currentLen;
 
-        public override string Name => "#US"; // Masking custom VM stream behind standard .NET User String stream name
+        public override string Name => "#NeonVM";
 
         public uint AddChunk(byte[] chunk)
         {
